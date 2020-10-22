8950c6fa41de11f63b1f57e91773296dd7ba176e
Miranda Ramirez Cospin <mxr17e@acu.edu>
2020-10-22 08:41:40 -0500

    MainStory board works!

---
+++ IRDb_JSONActivity-MRC/DataController.swift
@@ -0,0 +1,12 @@
+//
+//  DataController.swift
+//  IRDb_JSONActivity-MRC
+//
+//  Created by Miranda Ramirez Cospin on 10/22/20.
+//
+
+import UIKit
+
+class DataController: NSObject {
+
+}
---
+++ IRDb_JSONActivity-MRC/MediaDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  MediaDataModel.swift
+//  IRDb_JSONActivity-MRC
+//
+//  Created by Miranda Ramirez Cospin on 10/22/20.
+//
+
+import UIKit
+
+class MediaDataModel: NSObject {
+
+}
